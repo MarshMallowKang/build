88820a44fc691473f937671f8f8306e24a26b3cb
Stefano Mura [STELIX PippoX3] <STELIX@users.noreply.github.com>
2015-10-16 15:32:56 +0200

    build - roomservice: MarshMallowKang

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -43,7 +43,7 @@ default_team_rem = "aokp"
 # this shouldn't change unless google makes changes
 local_manifest_dir = ".repo/local_manifests"
 # change this to your name on github (or equivalent hosting)
-android_team = "A0KP"
+android_team = "MarshMallowKang"
 
 
 def check_repo_exists(git_data):
