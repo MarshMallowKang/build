739bdd485a2235570a2d5381a05c364595ad74d0
STELIX <ssspinni@gmail.com>
2015-10-27 21:58:32 +0100

    Build - Adjust device/brand shown during flash
    Change-Id: I46f9148bf70e15797a01ca0b3ef68b60dd725ca3
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -659,8 +659,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.Print("******************************************");
     script.Print("*   Compiled: %s"%(build));
 
-  device = GetBuildProp("ro.product.device", OPTIONS.info_dict)
-  model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
+  device = GetBuildProp("ro.aokp.device", OPTIONS.info_dict)
+  model = GetBuildProp("ro.product.brand", OPTIONS.info_dict)
   script.Print("*   Device: %s (%s)"%(model, device));
   script.Print("******************************************");
 
